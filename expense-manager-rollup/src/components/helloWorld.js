2607ece0458a71645630571c8cacd05cb7916cb2
Vickouma77 <Vickprestone20@gmail.com>
2023-12-20 13:55:14 +0300

    tutorials point

---
+++ expense-manager-rollup/src/components/helloWorld.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Hello World!</h1>
+         </div>
+      );
+   }
+}
+export default HelloWorld;
---
+++ expense-manager-rollup/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react';
+import HelloWorld from '../src/components/helloWorld';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <HelloWorld />
+    </React.StrictMode>
+    , document.getElementById('root')
+);
