24b3cb674b840fadc6bf432d82993ba3c4d1af06
Vickouma77 <Vickprestone20@gmail.com>
2024-01-03 15:01:11 +0300

    using blog list component

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,18 +1,17 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-
-    const [name, setName] = useState('mario');
-
-    const handleClick = () => {
-        setName('Luiigi');
-    }
+    const [blogs, setBlog] = useState([
+        { title: "My website", body: "lorem ipsum...", author: "Mario", id: 1 },
+        { title: "Welcome party !", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+        { title: "Web dev top tips", body: "lorem ipsum...", author: "Mario", id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h1>Homepage</h1>
-            <p>{ name }</p>
-            <button onClick={ handleClick }>Click Me</button>
+            <BlogList blogs={ blogs } title="All Blogs" />
+            <BlogList blogs={ blogs.filter((blog) => blog.author === "Mario") } title="Mario's Blog" />
         </div>
      );
 }
