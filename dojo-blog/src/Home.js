3968b5706ecfce79ebdb3a064a56f32f6489e58b
Vickouma77 <Vickprestone20@gmail.com>
2024-01-05 19:18:39 +0300

    conditional loading message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlog] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -11,11 +12,13 @@ const Home = () => {
         })
         .then(data => {
             setBlog(data);
+            setIsPending(false);
         })
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={ blogs } title="All Blogs" />}
         </div>
      );
