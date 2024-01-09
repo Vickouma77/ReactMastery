4538b7d39d259474862a504ce662ad92e28a6782
Vickouma77 <Vickprestone20@gmail.com>
2024-01-09 15:29:38 +0300

    using Route

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,16 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar'
 import Home from './Home'
+import Create from './Create';
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
 
       </div>
     </div>
+    </Router>
   );
 }
 
