7d593b4afb73797fbc80a7676137d79dd5ab59ac
Vickouma77 <Vickprestone20@gmail.com>
2024-01-09 15:35:05 +0300

    using Link

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <div className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">Blog</Link>
             </div>
         </div>
      );
