96b2d8ff620fd580d0db7eea48036886380db3d6
Vickouma77 <Vickprestone20@gmail.com>
2024-01-10 16:04:29 +0300

    handling Delete

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch('https://my-json-server.typicode.com/Vickouma77/demo/blogs/' + id);
+    const history = useHistory()
+
+    const handleClick = () => {
+        fetch('https://my-json-server.typicode.com/Vickouma77/demo/blogs/' + blog.id, {
+            method: "DELETE"
+        }).then(() => {
+            history.push('/')
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -15,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={ handleClick }>Delete</button>
                 </article>
             )}
         </div>
