c9f49ffc5421e8e7a84916e996b51aab9e5551e2
Vickouma77 <Vickprestone20@gmail.com>
2024-01-10 12:15:27 +0300

    adding component route

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar'
 import Home from './Home'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route path="/create">
             <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
           </Route>
         </Switch>
 
