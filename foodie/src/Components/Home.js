b5e70bf33b82ef287c156796c8ebed9bc65914cc
Vickouma77 <Vickprestone20@gmail.com>
2024-01-11 18:32:56 +0300

    creating Home

---
+++ foodie/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+const Home = () => {
+    return ( 
+        <div className='home-container'>
+            <Navbar />
+        </div>
+     );
+}
+ 
+export default Home;
+<div></div>
