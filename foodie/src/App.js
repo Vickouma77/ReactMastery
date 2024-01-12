4b6009627b2fa5dc9ab7052a3923babb711e2b2c
Vickouma77 <Vickprestone20@gmail.com>
2024-01-12 18:09:19 +0300

    adding work and about

--- foodie/src/App.js
+++ foodie/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import About from './Components/About';
 import Home from './Components/Home';
+import Work from './Components/Work';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
+      <Work />
     </div>
   );
 }
