04d4f50239b28eb125f25644af56b78499907a63
Vickouma77 <Vickprestone20@gmail.com>
2024-01-12 18:08:45 +0300

    adding about

---
+++ foodie/src/Components/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        <img src={AboutBackground} alt="" />
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          Food Is An Important Part Of A Balanced Diet
+        </h1>
+        <p className="primary-text">
+        Discover a world of culinary delight with Foodie! We're your go-to delivery service for hot, fresh, and irresistibly delicious meals. Elevate your dining experience effortlessly – because great food should come to you. Join us in savoring convenience, one bite at a time!
+        </p>
+
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
