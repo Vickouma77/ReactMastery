899dd92d4a5b188b7a764515693b696d17c58a64
Vickouma77 <Vickprestone20@gmail.com>
2024-01-19 14:55:35 +0300

    adding logo images

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => {
     return ( 
         <div className='nav-bar'>
             <Link className='logo' to='/' >
                 <img src={LogoS} alt='logo' />
+                <img className='sub-logo' src={LogoSubtitle} alt='logo' />
             </Link>
         </div>
      );
