408e813a54a7ec89701cee132de72c150c17f54a
Vickouma77 <Vickprestone20@gmail.com>
2024-04-11 12:37:29 +0300

    adding components

--- travel-react/src/App.js
+++ travel-react/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Destinations from './components/Destinations';
+import Search from './components/Search';
+import Selects from './components/Selects';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-    
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
+      <Selects />
+      <Carousel />
+      <Footer />
     </div>
   );
 }
